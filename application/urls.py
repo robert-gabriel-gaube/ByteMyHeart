ee2a89b4c7aa54c36806c77f415aeb1e426148d8
FermendjinPatricia <91600978+FermendjinPatricia@users.noreply.github.com>
2023-05-23 23:43:21 +0300

    BYT-27 Implement-UI-Design

--- application/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from application import views
-
-urlpatterns = [
-    path('', views.form)
-]
--- application/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-
-def form(request):
-    return render(request, "application/Form.html")
