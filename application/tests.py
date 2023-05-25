a43013d4ed62ab76622f5253b901864b6e066c7c
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2023-05-25 17:22:17 +0300

    BYT-62 Created 2 tests for reports action

--- application/tests.py
+++ application/tests.py
@@ -108,3 +108,89 @@ class ReportsShowTest(TestCase):
         ul_content = ul_content.replace('<ul>', '').replace('</ul>', '')
         
         self.assertEqual(ul_content.count('<li id="OPN">'), expected_li_count) 
+
+class ReportsActionTest(TestCase):
+    def setUp(self):
+        form = Form(
+            name='Robert',
+            age=20,
+            programming_language='C++',
+            university_specialization='CTI',
+            course_fav1="IC",
+            course_fav2="TS",
+            course_fav3="PAA",
+            hobby1='singing',
+            hobby2='cleaning',
+            hobby3='eating',
+            hobby4='sleeping',
+            hobby5='writing',
+            gender='M',
+            interest='M',
+            favorite_algorithm='rabin-karp',
+            favorite_data_structure='Binary Trees',
+            short_description='I like programming'
+        )
+        form.save()
+
+
+        user = User(
+            username='CandyButcher',
+            password=make_password("123456"),
+            matchId=None,
+            formId=form
+        )
+
+        user.save()
+
+        report = Report (
+            receiverID=user,
+            description="Toxic person"
+        )
+
+        report.save()
+
+    def test_reports_action_page(self):
+        report = Report.objects.get(pk=1)
+        self.assertEqual(report.status, "OPN")
+
+        user = User.objects.get(username='CandyButcher')
+        self.assertEqual(user.isBanned, False)
+
+        response = self.client.get('/reports/action/1/CandyButcher/1')
+        self.assertEqual(response.status_code, 302)
+
+        report = Report.objects.get(pk=1)
+        self.assertEqual(report.status, "CSD")
+
+        user = User.objects.get(username='CandyButcher')
+        self.assertEqual(user.isBanned, True)
+
+    def test_urls_of_reports_action_page(self):
+        response = self.client.get('/reports/action/1/CandyButcher/1')
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get('/reports/action/1/CandyButcher/0')
+        self.assertEqual(response.status_code, 302)
+
+        try:
+            response = self.client.get('/reports/action/1/CandyButcher/2')
+            gotPage = 1
+        except:
+            gotPage = 0
+
+        self.assertEqual(gotPage, 0)
+
+    def test_a_href_in_report_page(self):
+        response = self.client.get('/reports/action/1/CandyButcher/1')
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get('/reports')
+        self.assertEqual(response.status_code, 200)
+
+        content = response.content.decode('utf-8')
+
+        a_start_index = content.find('<a')
+        a_end_index = content.find('</a>')
+
+        a_content = content[a_start_index:a_end_index]
+        self.assertEqual(a_content[:11], '<a href="#"')
