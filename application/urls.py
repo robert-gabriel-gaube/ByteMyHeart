8c03fe7a6c8d73ab68004869ead639552c201531
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2023-05-25 16:18:27 +0300

    BYT-38 Added view and url for completing a report

--- application/urls.py
+++ application/urls.py
@@ -6,6 +6,6 @@ from application import views
 urlpatterns = [
     path('interestform', views.BigRegisterFormView.as_view()),
     path('register', views.UserRegisterFormView.as_view()),
-    path('reports', views.ReportsView.as_view())
-    
+    path('reports', views.ReportsView.as_view()),
+    path('reports/action/<int:pk>/<username>/<int:status>', views.ReportsActionView.as_view())
 ]
