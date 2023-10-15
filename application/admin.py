602d759421c43c29065f935fb29938edcbe0349c
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2023-10-15 17:15:54 +0300

    Added models to admin interface

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Form, User, Report
 
-# Register your models here.
+class UserModelAdmin(admin.ModelAdmin):
+    exclude = ('password',) 
+
+admin.site.register(Form)
+admin.site.register(User, UserModelAdmin)
+admin.site.register(Report)
